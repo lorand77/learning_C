88928ad2bef029b4878c07b1351eb2b8b0fcd883
lorand77 <lorand77@users.noreply.github.com>
2026-08-21 21:31:32 +0000

    initialize variables in test1.c and add circle_area.c for area calculation

---
+++ p02_variables/circle_area.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  double r = 2;
+  double area = r*r*3.1415;
+  printf("The area of the circle is: %f.\n", area);
+  return 0;
+}
+
--- p02_variables/test1.c
+++ p02_variables/test1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 int main() {
-  int a;
-  printf("The value of a is: %d\n", a);
+  int a = 1;
+  double b = 2.5;
+  char c[] = "Hello, World!\nHi there!";
+  printf("The value of a is: %d and the value of b is: %.12f and the value of c is: %s\n", a, b, c);
   return 0;
 }
+
