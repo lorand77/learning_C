5d25f3e24c70e13d65ad9f8c97a496318d558481
lorand <lorand77@users.noreply.github.com>
2026-08-26 09:12:22 +0000

    add loop examples with while and for constructs

--- p06_conditions/test1.c
+++ p06_conditions/test1.c
@@ -12,24 +12,24 @@ int main() {
     printf("x is not greater than y\n");
   }
 
-(x > y) ? printf("x is greater than y\n") : printf("x is not greater than y\n"); // too long
+  (x > y) ? printf("x is greater than y\n") : printf("x is not greater than y\n"); // too long
 
-if (x > 10) {
-  printf("x is greater than 10\n");
-  if (y > 10) {
-    printf("y is also greater than 10\n");
+  if (x > 10) {
+    printf("x is greater than 10\n");
+    if (y > 10) {
+      printf("y is also greater than 10\n");
+    }
   }
-}
 
-int isLoggedIn = 1;
-int isAdmin = 0;
-int securityLevel = 2; // 1 = highest
+  int isLoggedIn = 1;
+  int isAdmin = 0;
+  int securityLevel = 2; // 1 = highest
 
-if (isLoggedIn && (isAdmin || securityLevel <= 2) && !(securityLevel < 1)) {
-  printf("Access granted\n");
-} else {
-  printf("Access denied\n");
-}
+  if (isLoggedIn && (isAdmin || securityLevel <= 2) && !(securityLevel < 1)) {
+    printf("Access granted\n");
+  } else {
+    printf("Access denied\n");
+  }
 
   return 0;
 }
---
+++ p07_loops/test1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+  int i;
+  
+  i = 0;
+  while (i < 5) {
+    printf("%d\n", i);
+    i++;
+  }
+
+  // printf("after: %d\n", i);
+
+  return 0;
+}
