f108312c7ee1c3600b090b83e67b076f632585ac
lorand <lorand77@users.noreply.github.com>
2026-09-10 17:42:52 +0000

    functions and memory usage

--- p12_functions/sum-memsize.c
+++ p12_functions/sum-memsize.c
@@ -8,6 +8,8 @@ int sum(int a, int b) {
 int main() {
   int s = sum(1, 2);
   printf("sum = %d\n", s);
-    
+  
+  getchar();  // wait for Enter (no CPU) so ps/top can inspect VSZ and RSS
+  
   return 0;
 }
